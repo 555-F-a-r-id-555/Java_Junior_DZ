bf8d5bced9b2b5ddb1215d5865a9f637bc40ac04
Farid <makflay85zet@gmail.com>
2024-06-13 23:24:46 +0400

    DZ1 Department added equals and hashCode

--- src/main/java/org/example/DZ1/Department.java
+++ src/main/java/org/example/DZ1/Department.java
@@ -1,5 +1,7 @@
 package org.example.DZ1;
 
+import java.util.Objects;
+
 public class Department {
 
     private String name;
@@ -17,5 +19,18 @@ public class Department {
         return "Department{" +
                 "name='" + name + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
